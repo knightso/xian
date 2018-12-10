ff2cf325fd8f768f789094c817fa9820515f15e9
iii-ishida <dev.j.ishida@gmail.com>
2018-12-10 14:56:40 +0900

    add test(indexes, filters)
    - indexes
      - AddSomething
      - Build
      - MustBuild
      - IgnoreCase
    
    - filters
      - AddSomething
      - AddPrefix
      - Build
      - MustBuild
      - IgnoreCase

--- filters_test.go
+++ filters_test.go
@@ -1,6 +1,7 @@
 package xian
 
 import (
+	"fmt"
 	"reflect"
 	"sort"
 	"testing"
@@ -70,11 +71,38 @@ func TestAddBiunigramsFilter(t *testing.T) {
 	assertBuiltIndex(t, built, expected)
 }
 
+func TestAddPrefixFilter(t *testing.T) {
+	filter := NewFilters(nil)
+	filter.AddPrefix("label1", "abc dあいbCh")
+	filter.AddPrefix("label2", "abc debch iJあdeN")
+
+	built := filter.MustBuild()
+	assertBuiltFilter(t, built, []string{
+		"label1 abc dあいbCh",
+		"label2 abc debch iJあdeN",
+	})
+}
+
+func TestAddSomethingFilter(t *testing.T) {
+	filter := NewFilters(nil)
+	filter.AddSomething("label1", []string{"abc dあいbCh", "abc debch iJあdeN"})
+	filter.AddSomething("label2", 123)
+
+	built := filter.MustBuild()
+	assertBuiltFilter(t, built, []string{
+		"label1 abc dあいbCh",
+		"label1 abc debch iJあdeN",
+		"label2 123",
+	})
+}
+
 func TestAddAllFilter(t *testing.T) {
 	filter := NewFilters(nil)
 	filter.Add("label1", "abc dあいbCh", "sample")
 	filter.AddBigrams("label2", "abc dあいbCh")
 	filter.AddBiunigrams("label3", "abc dあいbCh")
+	filter.AddPrefix("label4", "abc dあいbCh")
+	filter.AddSomething("label5", []string{"abc dあいbCh", "AbcdeF"})
 
 	var expected []string
 
@@ -92,8 +120,106 @@ func TestAddAllFilter(t *testing.T) {
 		expected = append(expected, "label3 "+s)
 	}
 
+	// AddPrefix
+	expected = append(expected, "label4 abc dあいbCh")
+
+	// AddSomething
+	expected = append(expected, "label5 abc dあいbCh")
+	expected = append(expected, "label5 AbcdeF")
+
 	built := filter.MustBuild()
 	assertBuiltIndex(t, built, expected)
+}
+
+func TestBuildFilter(t *testing.T) {
+	t.Run("ConfigのCompositeIdxLabelsがMaxCompositeIndexLabelsより大きい場合", func(t *testing.T) {
+		labels := make([]string, MaxCompositeIndexLabels+1)
+		for i := 0; i < len(labels); i++ {
+			labels[i] = string('a' + i)
+		}
+
+		filter := NewFilters(&Config{CompositeIdxLabels: labels})
+		if _, err := filter.Build(); err == nil {
+			t.Error("error = nil, wants != nil")
+		}
+	})
+
+	t.Run("Buildの結果件数がMaxIndexesSizeより大きい場合", func(t *testing.T) {
+		filter := NewFilters(nil)
+		for i := 0; i < MaxIndexesSize+1; i++ {
+			filter.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		if _, err := filter.Build(); err == nil {
+			t.Error("error = nil, wants != nil")
+		}
+	})
+
+	t.Run("Config,結果件数が正常な場合", func(t *testing.T) {
+		filter := NewFilters(nil)
+		for i := 0; i < MaxIndexesSize; i++ {
+			filter.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		built, err := filter.Build()
+		if err != nil {
+			t.Errorf("error = %s, wants = nil", err)
+		}
+		if built == nil {
+			t.Error("built = nil, wants != nil")
+		}
+	})
+}
+
+func TestMustBuildFilter(t *testing.T) {
+	t.Run("ConfigのCompositeIdxLabelsがMaxCompositeIndexLabelsより大きい場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Error("expected:panic, was:not panic\n")
+			}
+		}()
+
+		labels := make([]string, MaxCompositeIndexLabels+1)
+		for i := 0; i < len(labels); i++ {
+			labels[i] = string('a' + i)
+		}
+
+		filter := NewFilters(&Config{CompositeIdxLabels: labels})
+		filter.MustBuild()
+	})
+
+	t.Run("Buildの結果件数がMaxIndexesSizeより大きい場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Error("expected:panic, was:not panic\n")
+			}
+		}()
+
+		filter := NewFilters(nil)
+		for i := 0; i < MaxIndexesSize+1; i++ {
+			filter.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		filter.MustBuild()
+	})
+
+	t.Run("Config,結果件数が正常な場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("expected:not panic, was:panic = [%v]\n", r)
+			}
+		}()
+
+		filter := NewFilters(nil)
+		for i := 0; i < MaxIndexesSize; i++ {
+			filter.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		built := filter.MustBuild()
+		if built == nil {
+			t.Error("built = nil, wants != nil")
+		}
+	})
 }
 
 func TestFilterConfigCompositeIdxLabels(t *testing.T) {
@@ -118,6 +244,41 @@ func TestFilterConfigCompositeIdxLabels(t *testing.T) {
 		// now needs only indexes with all specified labels
 		"7 a;b;c",
 	})
+}
+
+func TestFilterConfigIgnoreCase(t *testing.T) {
+	filter := NewFilters(&Config{IgnoreCase: true})
+	filter.Add("label1", "abc dあいbCh", "saMPle")
+	filter.AddBigrams("label2", "abc dあいbCh")
+	filter.AddBiunigrams("label3", "abc dあいbCh")
+	filter.AddPrefix("label4", "abc dあいbCh")
+	filter.AddSomething("label5", []string{"abc dあいbCh", "AbcdeF"})
+
+	var expected []string
+
+	// Add
+	expected = append(expected, "label1 abc dあいbch")
+	expected = append(expected, "label1 sample")
+
+	// AddBigrams
+	for _, s := range Bigrams("abc dあいbch") {
+		expected = append(expected, "label2 "+s)
+	}
+
+	// AddBiunigrams
+	for _, s := range Bigrams("abc dあいbch") {
+		expected = append(expected, "label3 "+s)
+	}
+
+	// AddPrefix
+	expected = append(expected, "label4 abc dあいbch")
+
+	// AddSomething
+	expected = append(expected, "label5 abc dあいbch")
+	expected = append(expected, "label5 abcdef")
+
+	built := filter.MustBuild()
+	assertBuiltIndex(t, built, expected)
 }
 
 func TestFilterConfigSaveNoFiltersIndex(t *testing.T) {
--- indexes_test.go
+++ indexes_test.go
@@ -1,6 +1,7 @@
 package xian
 
 import (
+	"fmt"
 	"reflect"
 	"sort"
 	"testing"
@@ -71,12 +72,26 @@ func TestAddPrefixesIndex(t *testing.T) {
 	assertBuiltIndex(t, built, expected)
 }
 
+func TestAddSomethingIndex(t *testing.T) {
+	idx := NewIndexes(nil)
+	idx.AddSomething("label1", []string{"abc dあいbCh", "abc debch iJあdeN"})
+	idx.AddSomething("label2", 123)
+
+	built := idx.MustBuild()
+	assertBuiltIndex(t, built, []string{
+		"label1 abc dあいbCh",
+		"label1 abc debch iJあdeN",
+		"label2 123",
+	})
+}
+
 func TestAddAllIndex(t *testing.T) {
 	idx := NewIndexes(nil)
 	idx.Add("label1", "abc dあいbCh", "sample")
 	idx.AddBigrams("label2", "abc dあいbCh")
 	idx.AddBiunigrams("label3", "abc dあいbCh")
 	idx.AddPrefixes("label4", "abc dあいbCh")
+	idx.AddSomething("label5", []string{"abc dあいbCh", "AbcdeF"})
 
 	var expected []string
 
@@ -99,8 +114,103 @@ func TestAddAllIndex(t *testing.T) {
 		expected = append(expected, "label4 "+s)
 	}
 
+	// AddSomething
+	expected = append(expected, "label5 abc dあいbCh")
+	expected = append(expected, "label5 AbcdeF")
+
 	built := idx.MustBuild()
 	assertBuiltIndex(t, built, expected)
+}
+
+func TestBuildIndex(t *testing.T) {
+	t.Run("ConfigのCompositeIdxLabelsがMaxCompositeIndexLabelsより大きい場合", func(t *testing.T) {
+		labels := make([]string, MaxCompositeIndexLabels+1)
+		for i := 0; i < len(labels); i++ {
+			labels[i] = string('a' + i)
+		}
+
+		idx := NewIndexes(&Config{CompositeIdxLabels: labels})
+		if _, err := idx.Build(); err == nil {
+			t.Error("error = nil, wants != nil")
+		}
+	})
+
+	t.Run("Buildの結果件数がMaxIndexesSizeより大きい場合", func(t *testing.T) {
+		idx := NewIndexes(nil)
+		for i := 0; i < MaxIndexesSize+1; i++ {
+			idx.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		if _, err := idx.Build(); err == nil {
+			t.Error("error = nil, wants != nil")
+		}
+	})
+
+	t.Run("Config,結果件数が正常な場合", func(t *testing.T) {
+		idx := NewIndexes(nil)
+		for i := 0; i < MaxIndexesSize; i++ {
+			idx.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		built, err := idx.Build()
+		if err != nil {
+			t.Errorf("error = %s, wants = nil", err)
+		}
+		if built == nil {
+			t.Error("built = nil, wants != nil")
+		}
+	})
+}
+
+func TestMustBuildIndex(t *testing.T) {
+	t.Run("ConfigのCompositeIdxLabelsがMaxCompositeIndexLabelsより大きい場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Error("expected:panic, was:not panic\n")
+			}
+		}()
+
+		labels := make([]string, MaxCompositeIndexLabels+1)
+		for i := 0; i < len(labels); i++ {
+			labels[i] = string('a' + i)
+		}
+
+		idx := NewIndexes(&Config{CompositeIdxLabels: labels})
+		idx.MustBuild()
+	})
+
+	t.Run("Buildの結果件数がMaxIndexesSizeより大きい場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Error("expected:panic, was:not panic\n")
+			}
+		}()
+
+		idx := NewIndexes(nil)
+		for i := 0; i < MaxIndexesSize+1; i++ {
+			idx.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		idx.MustBuild()
+	})
+
+	t.Run("Config,結果件数が正常な場合", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("expected:not panic, was:panic = [%v]\n", r)
+			}
+		}()
+
+		idx := NewIndexes(nil)
+		for i := 0; i < MaxIndexesSize; i++ {
+			idx.Add(fmt.Sprintf("label%d", i), "abc")
+		}
+
+		built := idx.MustBuild()
+		if built == nil {
+			t.Error("built = nil, wants != nil")
+		}
+	})
 }
 
 func TestIndexConfigCompositeIdxLabels(t *testing.T) {
@@ -128,6 +238,45 @@ func TestIndexConfigCompositeIdxLabels(t *testing.T) {
 		"6 b;c",
 		"7 a;b;c",
 	})
+}
+
+func TestIndexConfigIgnoreCase(t *testing.T) {
+	idx := NewIndexes(&Config{IgnoreCase: true})
+
+	idx.Add("label1", "abc dあいbCh", "saMPle")
+	idx.AddBigrams("label2", "abc dあいbCh")
+	idx.AddBiunigrams("label3", "abc dあいbCh")
+	idx.AddPrefixes("label4", "abc dあいbCh")
+	idx.AddSomething("label5", []string{"abc dあいbCh", "AbcdeF"})
+
+	var expected []string
+
+	// Add
+	expected = append(expected, "label1 abc dあいbch")
+	expected = append(expected, "label1 sample")
+
+	// AddBigrams
+	for _, s := range Bigrams("abc dあいbch") {
+		expected = append(expected, "label2 "+s)
+	}
+
+	// AddBiunigrams
+	for _, s := range Biunigrams("abc dあいbch") {
+		expected = append(expected, "label3 "+s)
+	}
+
+	// AddPrefixes
+	for _, s := range Prefixes("abc dあいbch") {
+		expected = append(expected, "label4 "+s)
+	}
+
+	// AddSomething
+	expected = append(expected, "label5 abc dあいbch")
+	expected = append(expected, "label5 abcdef")
+
+	built := idx.MustBuild()
+	assertBuiltIndex(t, built, expected)
+
 }
 
 func TestIndexConfigSaveNoFiltersIndex(t *testing.T) {
